7e4e3af4a1615d5dc0722e1a4695578525f546b3
FlutterHarshal <flutterharshal@gmail.com>
2024-08-11 21:40:04 +0530

    Intial Commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:notes/firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Notes"),
+        backgroundColor: Colors.blue,
+      ),
+      body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false) {
+                  print("You are verified");
+                } else {
+                  print("Not verified");
+                }
+                return  const Text("Done");
+              default:
+                return const Text('Loading');
+            }
+          }),
+    );
+  }
+}
