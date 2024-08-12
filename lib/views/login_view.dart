5df99dcf4264c73b0ab05d995ee6f42058a11557
FlutterHarshal <flutterharshal@gmail.com>
2024-08-13 00:55:32 +0530

    step-6

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'dart:developer' as devtools show log;
 import 'package:flutter/material.dart';
 import 'package:notes/constants/routers.dart';
+import 'package:notes/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -13,6 +13,7 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   late final TextEditingController _email;
   late final TextEditingController _password;
+
   @override
   void initState() {
     _email = TextEditingController();
@@ -37,60 +38,66 @@ class _LoginViewState extends State<LoginView> {
         ),
         backgroundColor: Colors.blue,
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: _email,
-            enableSuggestions: false,
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration:
-                const InputDecoration(hintText: 'Enter Your Email Here'),
-          ),
-          TextField(
-            controller: _password,
-            obscureText: true,
-            enableSuggestions: false,
-            autocorrect: false,
-            decoration:
-                const InputDecoration(hintText: 'Enter Your password Here'),
-          ),
-          TextButton(
-            onPressed: () async {
-              final email = _email.text;
-              final password = _password.text;
-              try {
-                await FirebaseAuth.instance.signInWithEmailAndPassword(
-                    email: email, password: password);
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _email,
+              enableSuggestions: false,
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration:
+                  const InputDecoration(hintText: 'Enter Your Email Here'),
+            ),
+            TextField(
+              controller: _password,
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+              decoration:
+                  const InputDecoration(hintText: 'Enter Your Password Here'),
+            ),
+            TextButton(
+              onPressed: () async {
+                final email = _email.text;
+                final password = _password.text;
+                try {
+                  await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: email, password: password);
 
-                Navigator.of(context)
-                    .pushNamedAndRemoveUntil(notesRoute, (route) => false);
-              } on FirebaseAuthException catch (e) {
-                if (e.code == 'user-not-found') {
-                  devtools.log('user not found');
-                } else if (e.code == 'wrong-password') {
-                  devtools.log('wrong password');
-                } else {
-                  devtools.log("Something bad Happened");
-                  devtools.log(e.toString());
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil(notesRoute, (route) => false);
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'user-not-found') {
+                    await showErrorDialog(context, "Error: User Not Found");
+                  } else if (e.code == 'invalid-credential') {
+                    await showErrorDialog(context, "Error: Wrong Credentials");
+                  } else {
+                    await showErrorDialog(
+                        context, "Error: An unknown error occurred ${e.code}");
+                  }
+                } catch (e) {
+                  await showErrorDialog(
+                      context, "${e.toString()}\nPlease try again.");
                 }
-              }
-            },
-            child: const Center(
-              child: Text(
-                "Login",
-                style: TextStyle(color: Colors.blue),
+              },
+              child: const Center(
+                child: Text(
+                  "Login",
+                  style: TextStyle(color: Colors.blue),
+                ),
               ),
             ),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context)
-                  .pushNamedAndRemoveUntil(registrationRoute, (route) => false);
-            },
-            child: const Text('Not Register yet? Register here! '),
-          ),
-        ],
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                    registrationRoute, (route) => false);
+              },
+              child: const Text('Not Registered yet? Register here!'),
+            ),
+          ],
+        ),
       ),
     );
   }
