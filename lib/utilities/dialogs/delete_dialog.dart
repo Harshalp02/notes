c3cf368f6d53ae1787015e2eb60ad1cfd20dbc31
FlutterHarshal <flutterharshal@gmail.com>
2024-08-17 12:08:02 +0530

    step-22

--- lib/utilities/dialogs/delete_dialog.dart
+++ lib/utilities/dialogs/delete_dialog.dart
@@ -4,7 +4,7 @@ import 'package:notes/utilities/dialogs/generic_dialog.dart';
 Future<bool> showDeleteDialog(BuildContext context) {
   return showGenericDialog<bool>(
           context: context,
-          title: 'Log out',
+          title: 'Delete',
           content: 'Are you sure you want to Delete this Dialog? ',
           optionBuilder: () => {'Cancel': false, 'Yes': true})
       .then((value) => value ?? false);
